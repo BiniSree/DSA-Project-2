01a14360fef4289b3f1b0249a4c6d38739aaa88e
BiniSree <147713207+BiniSree@users.noreply.github.com>
2024-02-24 09:55:15 +0000

    third

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import pickle
+import sklearn
 app = Flask(__name__)
 @app.route('/')
 def hello():
@@ -8,12 +9,14 @@ def hello():
 def prediction():
     if request.method == 'POST':
         category = request.form['category']
+        encoder1= pickle.load(open('encoder1.pkl','rb'))
+        category= encoder1.transform(category)
+        encder
         style = request.form['style']
         month = request.form['month']
         model_mdata_rf = pickle.load(open('model_mdata_rf.pkl','rb'))
         sales_count = model_mdata_rf.predict([['category','style','month']])
     return render_template('prediction.html', sales_count = sales_count)
-
 
 
 if __name__ == '__main__':
